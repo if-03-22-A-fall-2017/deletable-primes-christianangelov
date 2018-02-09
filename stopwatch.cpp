31dd1940130cdd4f6f7d5666236e8ab3513a7baa
christianangelov <31957091+christianangelov@users.noreply.github.com>
2018-02-09 19:06:17 +0100

    Add files via upload

---
+++ stopwatch.cpp
@@ -0,0 +1,35 @@
+/*-----------------------------------------------------------------------------
+ *				HTBLA-Leonding / Class: <your class name here>
+ *-----------------------------------------------------------------------------
+ * Exercise Number: #exercise_number#
+ * File:			stopwatch.cpp
+ * Author(s):		Peter Bauer
+ * Due Date:		#due#
+ *-----------------------------------------------------------------------------
+ * Description:
+ * <your description here>
+ *-----------------------------------------------------------------------------
+*/
+#include "stopwatch.h"
+#include <stdio.h>
+#include "time.h"
+clock_t start, end = 0;
+
+void start_stopwatch()
+{
+  start = clock();
+  end = 0;
+}
+
+void stop_stopwatch()
+{
+  end = clock();
+}
+float elapsed_time()
+{
+  if(end == 0)
+  {
+    stop_stopwatch();
+  }
+  return double(end-start)/ CLOCKS_PER_SEC * 1000;
+}
